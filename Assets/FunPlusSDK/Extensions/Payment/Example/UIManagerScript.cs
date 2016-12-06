c4fc16ec6637862d3e12277a4c60400a570d4274
yuankun.zhang <fmcocoa@gmail.com>
2016-12-06 14:05:20 +0800

    FunPlusPaymentClient

---
+++ Assets/FunPlusSDK/Extensions/Payment/Example/UIManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using FunPlus.Payment;
+
+public class UIManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FunPlusPaymentClient.SendData (
+			PaymentEnvironment.Sandbox,
+			PaymentChannel.Google,
+			"123",
+			"456",
+			"product_1",
+			"sig",
+			"signed",
+			"though_cargo"
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/FunPlusSDK/Extensions/Payment/FunPlusPaymentClient.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace FunPlus.Payment
+{
+	public enum PaymentEnvironment
+	{
+		Sandbox,
+		Production
+	}
+
+	public enum PaymentChannel
+	{
+		Google,
+		Apple
+	}
+
+	public class FunPlusPaymentClient : MonoBehaviour
+	{
+		private static FunPlusPaymentClient Instance { get; set; }
+
+		void Awake()
+		{
+			Instance = this;
+		}
+
+		private void SendDataToPaymentServer(PaymentEnvironment environment,
+											 PaymentChannel channel,
+											 string appId,
+											 string userId,
+											 string productId,
+											 string signature,
+											 string signedData,
+											 string throughCargo)
+		{
+			string url = GetPaymentServerUrl (environment, channel);
+
+			WWWForm wf = new WWWForm ();
+			wf.AddField ("appid", appId);
+			wf.AddField ("uid", userId);
+
+			StartCoroutine (Post (url, wf, null, null));
+		}
+
+		private IEnumerator Post(string url, WWWForm wf, Action<Hashtable> onSuccess, Action<Hashtable> onFailure)
+		{
+			WWW www = new WWW (url, wf);
+			Debug.Log (url);
+			yield return www;
+
+			if (www == null || www.error != null || !www.isDone)
+			{
+				Debug.LogWarning ("[FunPlusSDK] Failed to request to payment server.");
+			}
+			else if (string.IsNullOrEmpty (www.text))
+			{
+				Debug.LogWarning ("[FunPlusSDK] Invalid response from payment server.");
+			}
+			else
+			{
+				Debug.Log(www.text);
+			}
+		}
+
+		private static string GetPaymentServerUrl(PaymentEnvironment environment, PaymentChannel channel)
+		{
+			string host = environment.Equals (PaymentEnvironment.Sandbox) ?
+				"https://payment-sandbox.funplusgame.com" :
+				"https://payment.funplusgame.com";
+
+			string channelName = channel.Equals (PaymentChannel.Google) ?
+				"googleplayiap" :
+				channel.Equals (PaymentChannel.Apple) ?
+				"appleiap" :
+				"unknown";
+
+			return string.Format ("{0}/callback/{1}/", host, channelName);
+		}
+
+		public static void SendData(PaymentEnvironment environment,
+									PaymentChannel channel,
+								    string appId,
+							 	    string userId,
+						   		    string productId,
+									string signature,
+									string signedData,
+									string throughCargo)
+		{
+			Instance.SendDataToPaymentServer (
+				environment,
+				channel,
+				appId,
+				userId,
+				productId,
+				signature,
+				signedData,
+				throughCargo
+			);
+		}
+	}
+
+}
